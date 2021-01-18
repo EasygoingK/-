cb59a375e7cd5b3e75c350df8fd73504aa33cd00
Kevin <kevin@gmail.com>
2021-01-18 22:38:54 +0800

    完成ADO.NET連線資料庫，取得病患清單顯示於首頁。

--- ReportAssign/Controllers/AssignController.cs
+++ ReportAssign/Controllers/AssignController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using ReportAssign.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace ReportAssign.Controllers
@@ -11,6 +14,63 @@ namespace ReportAssign.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult QueryReport(string accession_num)
+        {
+
+            if (accession_num != null)
+            {
+
+                List<PatientList> patlist = new List<PatientList>();
+
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["ReportDB"].ConnectionString))
+                {
+                    conn.Open();
+
+                    string sql = "SELECT PatientID, PatientName, AccessionNum, DoctorID, DoctorName " +
+                        "FROM patientlist Where AccessionNum = @AccessionNum";
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+
+                    cmd.Parameters.AddWithValue("@AccessionNum", accession_num);
+
+                    SqlDataReader Reader = cmd.ExecuteReader();
+
+                    if (Reader.HasRows)
+                    {
+                        if (Reader.Read())
+                        {
+
+                            patlist.Add(new PatientList()
+                            {
+                                PatientID = Convert.ToString(Reader[0]),
+                                PatientName = Convert.ToString(Reader[1]),
+                                AccessionNum = Convert.ToString(Reader[2]),
+                                DoctorID = Convert.ToString(Reader[3]),
+                                DoctorName = Convert.ToString(Reader[4])
+                            });
+
+                            if (patlist.Count >= 1)
+                            {
+                                ViewBag.CheckData = "ok";
+                            }
+
+                        }
+                    }
+
+
+                }
+
+                return View("Index", patlist);
+
+            }
+            else
+            {
+                return View("Index");
+            }
+
         }
     }
 }
